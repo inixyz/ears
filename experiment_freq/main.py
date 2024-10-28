00e6a770db9688ce840f3d9a372c988e67d0e2db
inixyz <alexandru.ene2702@gmail.com>
2024-10-28 14:58:24 +0200

    finallu something works

--- experiment_freq/main.py
+++ experiment_freq/main.py
@@ -5,6 +5,7 @@ import tqdm
 from scipy.io import wavfile
 from scipy.signal import resample_poly
 from numba import jit, prange
+import argparse
 
 c = [343, 1000]
 dx = 0.01
@@ -16,11 +17,6 @@ mat_const = [(c_mat**2 * dt**2 / dx**2) for c_mat in c]
 dim_x, dim_y = 500, 500
 u = np.zeros((3, dim_x, dim_y))
 m = np.zeros((dim_x, dim_y))
-
-# world data
-m[100:300, 100:300] = 1
-source_x, source_y = 200, 350
-record_x, record_y = 70, 70
 
 
 @jit(nopython=True, parallel=True)
@@ -57,11 +53,19 @@ def scale_back(arr):
     return scaled_arr
 
 
-def main():
-    sample_rate, input_signal = wavfile.read("samples/impulse.wav")
+def main(args):
+    global source_x, source_y, record_x, record_y
+    source_x, source_y = args.source_pos
+    record_x, record_y = args.receiver_pos
+
+    # Set materials rectangle
+    mat_x_start, mat_x_end, mat_y_start, mat_y_end = args.materials_rect
+    m[mat_x_start:mat_x_end, mat_y_start:mat_y_end] = 1
+
+    sample_rate, input_signal = wavfile.read(args.input_wav)
     input_signal = input_signal / np.max(np.abs(input_signal))  # normalize
 
-    # resample
+    # Resample
     target_sample_rate = int(1 / dt)
     gcd = np.gcd(sample_rate, target_sample_rate)
     up = target_sample_rate // gcd
@@ -90,16 +94,51 @@ def main():
 
         recorded_signal.append(u[0, record_x, record_y])
 
-        if t % 100 == 0:
+        if args.visualize and t % 100 == 0:
             display(fig, ax, im_air, im_material, t)
 
     recorded_signal = scale_back(recorded_signal)
     recorded_signal = np.array(recorded_signal) * 32767
     recorded_signal = recorded_signal.astype(np.int16)
-    wavfile.write("samples/1_recorded.wav", target_sample_rate, recorded_signal)
+    wavfile.write(args.output_wav, target_sample_rate, recorded_signal)
 
-    plt.show()
+    if args.visualize:
+        plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Wave Propagation Simulation")
+    parser.add_argument(
+        "--source_pos",
+        type=int,
+        nargs=2,
+        default=[200, 350],
+        help="Source position (x, y)",
+    )
+    parser.add_argument(
+        "--receiver_pos",
+        type=int,
+        nargs=2,
+        default=[70, 70],
+        help="Receiver position (x, y)",
+    )
+    parser.add_argument(
+        "--materials_rect",
+        type=int,
+        nargs=4,
+        default=[100, 300, 100, 300],
+        help="Materials rectangle (x_start, x_end, y_start, y_end)",
+    )
+    parser.add_argument(
+        "--input_wav", type=str, default="samples/impulse.wav", help="Input WAV file"
+    )
+    parser.add_argument(
+        "--output_wav",
+        type=str,
+        default="samples/1_recorded.wav",
+        help="Output WAV file",
+    )
+    parser.add_argument("--visualize", action="store_true", help="Enable visualization")
+
+    args = parser.parse_args()
+    main(args)
