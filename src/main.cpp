fb52b426e326ea5f7bf26113fea03126c0fa35db
inixyz <alexandru.ene2702@gmail.com>
2025-02-24 17:41:26 +0200

    heatmap is gucci

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,79 @@
 #include "imgui.h"
+#include "implot.h"
 #include "rlImGui.h"
 #include "world.cuh"
 #include <raylib.h>
+#include <vector>
 
-void menu(World &world) {
-  ImGui::Begin("boss");
-  if (ImGui::Button("Step"))
-    world.step();
-  if (ImGui::Button("ZERO"))
-    world.set_t0(Vec3i(16, 16, 16), 0);
+std::vector<float> saved_signal;
+
+void heatmap(const char *const label, const World &world, const int pos_y) {
+  const int size_x = world.get_size().x;
+  const int size_z = world.get_size().z;
+
+  float grid[size_x][size_z];
+
+  for (int x = 0; x < size_x; x++)
+    for (int z = 0; z < size_z; z++)
+      grid[x][z] = world.get_t0(Vec3i(x, pos_y, z));
+
+  static ImPlotAxisFlags axes_flags =
+      ImPlotAxisFlags_Lock | ImPlotAxisFlags_NoGridLines | ImPlotAxisFlags_NoTickMarks;
+
+  ImGui::Begin(label);
+
+  static char format[20] = "%g";
+  ImGui::InputText("format:", format, 20);
+
+  ImPlot::PushColormap(ImPlotColormap_Viridis);
+  if (ImPlot::BeginPlot(label, ImVec2(-1, -1))) {
+    ImPlot::PlotHeatmap(label, grid[0], size_x, size_z, -1, 1, format);
+    ImPlot::EndPlot();
+  }
+  ImPlot::PopColormap();
   ImGui::End();
 }
 
-void draw_world(World &world) {
-  const Vec3i size = world.get_size();
-  const float spacing_distance = world.get_spacing_distance();
-  const Vector3 cube_size = {spacing_distance, spacing_distance, spacing_distance};
+void menu(World &world) {
+  ImGui::Begin("control");
+  if (ImGui::Button("step")) {
+    world.step(1000000);
+    saved_signal.push_back(world.get_t0(Vec3i(10, 10, 25)));
+  }
+  ImGui::End();
 
-  int y = 16;
-  for (int x = 0; x < size.x; x++)
-    // for (int y = 0; y < size.y; y++)
-    for (int z = 0; z < size.z; z++) {
-      const Vector3 pos = {x * spacing_distance, y * spacing_distance, z * spacing_distance};
-      const float val = world.get_t0(Vec3i(x, y, z));
-      if (val == 0)
-        continue;
-      const Color color = {0, static_cast<unsigned char>(val * 127 + 127), 0, 255};
-      DrawCubeV(pos, cube_size, color);
-    }
+  heatmap("y=16", world, 25);
+
+  ImGui::Begin("saved signal");
+  if (ImPlot::BeginPlot("saved signal")) {
+    ImPlot::PlotLine("##saved_signal", saved_signal.data(), saved_signal.size());
+    ImPlot::EndPlot();
+  }
+  ImGui::End();
 }
 
 int main() {
+  // init raylib and window
   SetTraceLogLevel(LOG_WARNING);
-  InitWindow(2000, 2000, "app");
+  InitWindow(1000, 1000, "app");
 
-  rlImGuiSetup(false);
-  constexpr float menu_scale_factor = 1.5;
+  // init dear imgui
+  rlImGuiSetup(true);
+  ImPlot::CreateContext();
+  constexpr float menu_scale_factor = 1;
   ImGui::GetIO().FontGlobalScale = menu_scale_factor;
   ImGui::GetStyle().ScaleAllSizes(menu_scale_factor);
 
-  Camera3D camera{};
-  camera.position = {10, 10, 10};
-  camera.target = {0, 0, 0};
-  camera.up = {0, 1, 0};
-  camera.fovy = 60;
-  camera.projection = CAMERA_PERSPECTIVE;
-
   // initial state
-  World world(Vec3i(32, 32, 32), 0.1, Vec3i(4, 4, 4), Vec3i(8, 8, 8));
-  world.materials[0] = {343, 0.5};
+  World world(Vec3i(50, 50, 50), 0.1, Vec3i(5, 5, 5), Vec3i(10, 10, 10));
+  world.materials[0] = {343, 1};
 
   world.compute_material_attributes();
-  world.set_t0(Vec3i(16, 16, 16), 1);
+  world.set_t0(Vec3i(25, 25, 25), 100);
 
   while (!WindowShouldClose()) {
-    if (IsWindowFocused() && IsMouseButtonDown(MOUSE_LEFT_BUTTON))
-      UpdateCamera(&camera, CAMERA_FREE);
-
     BeginDrawing();
     ClearBackground(GRAY);
-
-    BeginMode3D(camera);
-    draw_world(world);
-    EndMode3D();
 
     rlImGuiBegin();
     menu(world);
@@ -72,6 +83,7 @@ int main() {
     EndDrawing();
   }
 
+  ImPlot::DestroyContext();
   rlImGuiShutdown();
   CloseWindow();
 }
